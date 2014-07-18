dbb6ba06e6c8c6bfedc8935215072c349d417fcd
akshay bansal <akshaybansal20@gmail.com>
2014-07-18 15:19:25 +0530

    Revert "Revert "files""
    This reverts commit 0e0e6dd0dd5c16c01edb1e913a532d97310544de.

---
+++ global_variables.py
@@ -0,0 +1,5 @@
+from re import compile
+negation_list = ['don\'t', 'not', 'never']
+# tasks to be done in the constructor itself!
+subs_list = [compile("\'ve"), compile("\'d"), compile("\'s")]
+replace_list = ["have", "would", "is"]
